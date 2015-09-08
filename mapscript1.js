e355735c28002f6878a5eb9abb105ea3e48479d1
Ben Davis <benjaminedavis@gmail.com>
2015-09-08 06:23:38 -0700

    +map marker arrows +geolocation, but not working

--- mapscript1.js
+++ mapscript1.js
@@ -99,4 +99,52 @@ var map = new google.maps.Map(document.getElementById('map'), {
   styles: styleArray,
   zoom: 16
 });
+
+var infoWindow = new google.maps.InfoWindow({map: map});
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Your Location.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  };
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+};
+
+// Create a black down arrow marker at the same lat and lng.
+var marker1 = new google.maps.Marker({
+    position: {lat: 34.017, lng: 241.502},
+    icon: {
+      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+      scale: 4
+    },
+    map: map,
+  });
+
+var marker2 = new google.maps.Marker({
+    position: {lat: 34.0157, lng: 241.506},
+    icon: {
+      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+      scale: 4
+    },
+    map: map,
+  });
+
 }
